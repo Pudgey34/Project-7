28874b358aae0b46c67f97e2e49aad8b034938a5
Pudgey34 <stephenwilton@gmail.com>
2026-02-02 21:19:44 -0500

    Arena Update

---
+++ src/scenes/arena/arena.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+class_name Arena
+
+@export var player: Player
+
+func _ready() -> void:
+	Global.player = player
---
+++ src/scenes/arena/camera_2d.gd
@@ -0,0 +1,8 @@
+extends Camera2D
+
+class_name Camera
+
+func _process(delta: float) -> void:
+	if is_instance_valid(Global.player):
+		global_position = Global.player.global_position
+		
