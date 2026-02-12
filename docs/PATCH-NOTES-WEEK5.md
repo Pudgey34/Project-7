b3ab810959da39c6eaac0e99815b40e9a7a5bb54
Pudgey34 <stephenwilton@gmail.com>
2026-02-11 20:13:35 -0500

    Punch Stats Added

---
+++ docs/PATCH-NOTES-WEEK5.md
@@ -0,0 +1,2 @@
+7:30PM to 10:00 PM
+Melee attacks implemented
--- src/autoloads/global.gd
+++ src/autoloads/global.gd
@@ -7,6 +7,13 @@ signal on_create_damage_text(unit: Node2D, hitbox: HitboxComponent)
 const FLASH_MATERIAL = preload("uid://bys1rfyuawvhe")
 const FLOATING_TEXT_SCENE = preload("uid://cnjxlvdplj5ds")
 
+enum UpgradeTier{
+	COMMON,
+	RARE,
+	EPIC,
+	LEGENDARY
+}
+
 var player: Player
 
 func get_chance_success(chance: float) -> bool:
